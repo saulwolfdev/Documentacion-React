e77eca21c920acc739b4e2b01c7b12046e34743b
Saul <estrellasoscuras1984@gmail.com>
2023-11-13 14:20:06 -0300

    se agrega codesanboxlink

--- src/components-mdx/CodeBlock.js
+++ src/components-mdx/CodeBlock.js
@@ -2,37 +2,39 @@ import React, {useState} from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Button } from './index';
+import { Button, CodeSandboxLink } from './index';
 
-const CodeBlock = ({ codeString, language }) => {
-    const [copied, setCopied] = useState(false);
-  
-    const handleCopy = () => {
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000);
-    };
-  
-    const copyButtonLabel = copied ? 'Copied' : 'Copy';
-    const primary = copied ? true : false;
-  
-    const copyButton = <Button primary={primary} label={copyButtonLabel} size="small" />;
-  
-    return (
-      <div>
-        <SyntaxHighlighter language={language || "javascript"} style={materialDark}>
-          {codeString}
-        </SyntaxHighlighter>
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <CopyToClipboard text={codeString} onCopy={handleCopy}>
-        {copyButton}
-        </CopyToClipboard>
-        </div>
-      </div>
-    );
+const CodeBlock = ({ codeString, language, sandboxUrl, showButtons }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
   };
-  
-  export default CodeBlock;
-  
+
+  const copyButtonLabel = copied ? 'Copied' : 'Copy';
+  const primary = copied ? true : false;
+
+  const copyButton = <Button primary={primary} label={copyButtonLabel} size="small" />;
+
+  return (
+    <div>
+      <SyntaxHighlighter language={language || "javascript"} style={materialDark}>
+        {codeString}
+      </SyntaxHighlighter>
+      
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        {showButtons && (<CodeSandboxLink url={sandboxUrl} />)}
+          <CopyToClipboard text={codeString} onCopy={handleCopy}>
+            {copyButton}
+          </CopyToClipboard>
+        </div>
+      
+    </div>
+  );
+};
+
+export default CodeBlock;
 
 
 
