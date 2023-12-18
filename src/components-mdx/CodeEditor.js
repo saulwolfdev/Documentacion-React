1aed970710f67a111544c2f1952b321fe4764ad1
Saul <estrellasoscuras1984@gmail.com>
2023-12-18 09:06:01 -0300

    prueba de react-live

--- src/components-mdx/CodeEditor.js
+++ src/components-mdx/CodeEditor.js
@@ -4,14 +4,14 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 const CodeEditor = ({ codeString }) => {
     // const modifiedCode = `${initialCode}`;
   return (
-    <LiveProvider code={codeString || ''} >
-      <div style={{ display: 'flex', height: '50vh' }}>
+    <LiveProvider code={codeString || ''} noInline>
+      <div style={{ display: 'flex', height: '50vh', padding: '16px 0 12px 0' }}>
         <div style={{ flex: 1, overflow: 'hidden' }}>
-          <LiveEditor style={{ height: '100%', border: 'none', boxSizing: 'border-box', margin: '10px', overflow: 'auto' }} />
+          <LiveEditor style={{ height: '100%', border: 'none', boxSizing: 'border-box', overflow: 'auto' }} />
           <LiveError />
         </div>
         <div style={{ flex: 1, overflow: 'hidden' }}>
-          <LivePreview style={{ width: '100%', height: '100%', border: 'none', boxSizing: 'border-box', margin: '10px', fontFamily: 'monospace' }} />
+          <LivePreview style={{ width: '100%', height: '100%', border: 'none', boxSizing: 'border-box', marginLeft: '8px', fontFamily: 'monospace' }} />
         </div>
       </div>
     </LiveProvider>
