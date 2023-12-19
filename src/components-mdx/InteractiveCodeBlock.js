7d2f9e14bc6e11c3af90094ccc19bf5bf7d9c967
Saul <estrellasoscuras1984@gmail.com>
2023-12-19 10:01:35 -0300

    se agrega documentacion de estilos

--- src/components-mdx/InteractiveCodeBlock.js
+++ src/components-mdx/InteractiveCodeBlock.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { CodeBlock, CodeEditor } from './index';
 
-const InteractiveCodeBlock = ({ codeString, language, sandboxUrl, showButtons, showCodeBlock=false , showCodeEditor }) => {
+const InteractiveCodeBlock = ({ codeString, language, sandboxUrl, showButtons, showCodeBlock , showCodeEditor, noInline }) => {
   return (
     <div>
         {showCodeBlock ? (
           <CodeBlock codeString={codeString} language={language} sandboxUrl={sandboxUrl} showButtons={showButtons} />
         ) : null}
         {showCodeEditor ? (
-          <CodeEditor codeString={codeString} />
+          <CodeEditor codeString={codeString} noInline={noInline} />
         ) : null}
     </div>
   );
