657f95f3fd6aa2522174640da566f605c46cec5b
Saul <estrellasoscuras1984@gmail.com>
2023-12-19 10:11:20 -0300

    se agrega la condicion noInline al componente codeEditor

--- src/components-mdx/CodeEditor.js
+++ src/components-mdx/CodeEditor.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-const CodeEditor = ({ codeString }) => {
-    // const modifiedCode = `${initialCode}`;
+const CodeEditor = ({ codeString, noInline }) => {
   return (
-    <LiveProvider code={codeString || ''} noInline>
+    <LiveProvider code={codeString || ''} noInline={noInline}>
       <div style={{ display: 'flex', height: 'auto', padding: '16px 0 12px 0' }}>
         <div style={{ flex: 1, overflow: 'hidden' }}>
           <LiveEditor style={{ height: '100%', border: 'none', boxSizing: 'border-box', overflow: 'auto' }} />
