d5305af2df70862e76e44feb626fef460908be4a
Saul <estrellasoscuras1984@gmail.com>
2023-12-19 14:52:13 -0300

    se agrega concepto de formulario en react

--- src/components-mdx/CodeEditor.js
+++ src/components-mdx/CodeEditor.js
@@ -5,11 +5,11 @@ const CodeEditor = ({ codeString, noInline }) => {
   return (
     <LiveProvider code={codeString || ''} noInline={noInline}>
       <div style={{ display: 'flex', height: 'auto', padding: '16px 0 12px 0' }}>
-        <div style={{ flex: 1, overflow: 'hidden' }}>
+        <div style={{ flex: '1 1 20%', overflow: 'hidden' }}>
           <LiveEditor style={{ height: '100%', border: 'none', boxSizing: 'border-box', overflow: 'auto' }} />
           <LiveError />
         </div>
-        <div style={{ flex: 1, overflow: 'hidden', textAlign: 'center' }}>
+        <div style={{ flex: 1, overflow: 'auto', textAlign: 'center' }}>
           <LivePreview style={{ width: '100%', height: '100%', border: 'none', boxSizing: 'border-box', marginLeft: '8px', fontFamily: 'monospace' }} />
         </div>
       </div>
