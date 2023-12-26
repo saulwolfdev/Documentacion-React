0a05d231533c0f5ad36d4d95a7e7e6b915870d97
Saul <estrellasoscuras1984@gmail.com>
2023-12-26 13:30:06 -0300

    se crea el componente SanPack

--- src/components-mdx/SanPack.js
+++ src/components-mdx/SanPack.js
@@ -1,67 +1,34 @@
 import React from 'react';
 import {
-    SandpackProvider,
-    SandpackLayout,
-    SandpackCodeEditor,
-    SandpackPreview,
-    SandpackFileExplorer,
-  } from "@codesandbox/sandpack-react";
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
+import { amethyst } from "@codesandbox/sandpack-themes";
 
-const SandPack = () => {
-  
+const SandPack = ({ files }) => {
   return (
     <div style={{ width: '900px' }}> 
-    <SandpackProvider
-    theme="light" 
-    template="react"
-    files={{ 
-      "/Button.js": 
-`import React from "react";
-
-const Button=()=> {
-   const handleClick = () => {
-    alert("hola");
-  };
-  return <button onClick={handleClick}>hola</button>;
-}
-
-export default Button;`,
-
-"Style.css":
-`.button {
-  background-color: #3498db;
-  color: #fff;
-  font-size: 16px;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-}`,
-        
-"App.js": 
-`import React from "react";
-import Button from "./Button.js";
-
-const App=()=> {
-  return (
-    <div>
-      <Button />
-    </div>
-  );
-}
-
-export default App`
-    }}
-    options={{
-      visibleFiles: ["/App.js", "/Button.js", "/Style.css"],
-
-    }} 
-    >
-      <SandpackLayout>
-        <SandpackFileExplorer />
-        <SandpackCodeEditor closableTabs showTabs />
-        <SandpackPreview />
-      </SandpackLayout>
-    </SandpackProvider>
+      <SandpackProvider
+        theme={amethyst} 
+        template="react"
+        customSetup={{
+          dependencies: {
+            "css": "latest",
+            "styled-components": "latest",
+            "react-dom": "latest",
+          },
+        }}
+        files={files}
+      >
+        <SandpackLayout>
+          <SandpackFileExplorer autoHiddenFiles/>
+          <SandpackCodeEditor closableTabs showTabs />
+          <SandpackPreview />
+        </SandpackLayout>
+      </SandpackProvider>
     </div>
   );
 };
