d817acd7a0836eba65428dbadd7e5bc1727a727f
Saul <estrellasoscuras1984@gmail.com>
2024-01-11 18:24:40 -0300

    prueba de tab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { ThemeProvider } from '@mui/material';
 import theme  from './material-ui/theme';
-import ButtonComponent  from './material-ui/button';
+import ButtonComponent  from './material-ui/ButtonComponent';
 import CardComponent  from './material-ui/CardComponent';
 import SelectComponent from './material-ui/SelectComponent';
-
+import TabsComponent from './material-ui/TabsComponent';
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
       <ButtonComponent label='button' variant="contained" color="colorCustom" onClick={()=> console.log('click')} />
       <CardComponent />
       <SelectComponent/>
+      <TabsComponent />
     </ThemeProvider>
   )
 }
