eb2dfd01d4bd8e605edf457f67fe4d7eba1dcd41
Saul <estrellasoscuras1984@gmail.com>
2024-01-11 12:47:26 -0300

    se agrega la libreria MUI y se agrega un theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import { Button, ThemeProvider, createTheme } from '@mui/material';
+import theme  from './material-ui/theme';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div>
+        hola       
+      </div>
+      <Button 
+      variant='contained' 
+      color='success'
+      onClick={()=>{alert('click')}}
+      >
+        button
+      </Button>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/material-ui/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#000e35",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+    },
+  });
+  
+  export default theme
